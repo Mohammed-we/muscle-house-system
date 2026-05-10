a6e6c975eee7e3ca5969350a2d948560311803db
mohammedshaaban2004 <m45470123@gmail.com>
2026-05-11 01:24:32 +0300

    Clean up code and fix build configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development' // يعطل في وضع التطوير لتسهيل العمل
-// });
 
-// const nextConfig = withPWA({
-//   // إعداداتك الأصلية هنا
-// });
-
-// module.exports = nextConfig;
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
